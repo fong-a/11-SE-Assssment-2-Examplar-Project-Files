7e9714055b61359281cde0c0ad58d3f75df6b76e
Mr Fong <fong.a@saintaug.nsw.edu.au>
2023-11-12 14:37:24 +1100

    Started UserInterface class and created main window

--- user_interface.py
+++ user_interface.py
@@ -1,4 +1,18 @@
+import gooeypie as gp
+
+
 class UserInterface:
     def __init__(self) -> None:
-        width = 500
-        height = 300
+        self.width = 500
+        self.height = 300
+
+    def render_main_window(self):
+        app = gp.GooeyPieApp("LockSmithy")  # title = "LockSmithy"
+        app.width = self.width  # width = 500
+        app.height = self.height  # height = 300
+
+        app.run()
+
+
+main = UserInterface()
+main.render_main_window()
