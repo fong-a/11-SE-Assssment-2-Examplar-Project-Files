797ef33a864e03b41eb207dff39bd03096aa4793
Mr Fong <fong.a@saintaug.nsw.edu.au>
2023-11-12 17:48:42 +1100

    Happy with the styled label. Font looks good.

--- user_interface.py
+++ user_interface.py
@@ -10,6 +10,15 @@ class UserInterface:
         app = gp.GooeyPieApp("LockSmithy")  # title = "LockSmithy"
         app.width = self.width  # width = 500
         app.height = self.height  # height = 300
+        app.set_grid(7, 4)  # set_grid(rows = 7, columns = 4)
+
+        # create title text
+        styled_label = gp.StyleLabel(app, "LockSmithy")
+        styled_label.font_style = "normal"
+        styled_label.font_weight = "bold"
+        styled_label.font_name = "Helvetica"
+        styled_label.font_size = 40
+        app.add(styled_label, 1, 1)
 
         app.run()
 
