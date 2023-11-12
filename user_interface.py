dd8e03fea177dca0bf891e6329e0feb8f6b8eeb6
Mr Fong <fong.a@saintaug.nsw.edu.au>
2023-11-12 17:56:06 +1100

    Added subtitle but need to sort out positioning and grid.

--- user_interface.py
+++ user_interface.py
@@ -10,15 +10,22 @@ class UserInterface:
         app = gp.GooeyPieApp("LockSmithy")  # title = "LockSmithy"
         app.width = self.width  # width = 500
         app.height = self.height  # height = 300
-        app.set_grid(7, 4)  # set_grid(rows = 7, columns = 4)
+        app.set_grid(2, 2)  # set_grid(rows = 7, columns = 4)
 
         # create title text
-        styled_label = gp.StyleLabel(app, "LockSmithy")
-        styled_label.font_style = "normal"
-        styled_label.font_weight = "bold"
-        styled_label.font_name = "Helvetica"
-        styled_label.font_size = 40
-        app.add(styled_label, 1, 1)
+        title_label = gp.StyleLabel(app, "LockSmithy")
+        title_label.font_weight = "bold"
+        title_label.font_name = "Helvetica"
+        title_label.font_size = 10
+        app.add(title_label, 1, 1)
+
+        # create subtitle text
+        subtitle_label = gp.StyleLabel(app, "Forging strong passwords since 2023")
+        title_label.font_style = "normal"
+        title_label.font_weight = "bold"
+        title_label.font_name = "Helvetica"
+        title_label.font_size = 20
+        app.add(subtitle_label, 2, 1)
 
         app.run()
 
